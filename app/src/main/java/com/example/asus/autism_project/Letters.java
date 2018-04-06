e172f4218e9568a4cf813c30cbcf23194b04eeb5
Yeaseen <1405091.ya@ugrad.cse.buet.ac.bd>
2018-04-06 06:26:25 +0600

    update on letters 2nd

--- app/src/main/java/com/example/asus/autism_project/Letters.java
+++ app/src/main/java/com/example/asus/autism_project/Letters.java
@@ -1,17 +1,24 @@
 package com.example.asus.autism_project;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Letters extends AppCompatActivity {
     public static ImageButton banana_btn;
+    public static ImageButton cat_btn;
+    public static ImageButton elephant_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letters);
+        onClickbananaBtnListener();
+        onClickcatBtnListener();
+        onClickelephantBtnListener();
     }
 
     public void onClickbananaBtnListener(){
@@ -20,10 +27,36 @@ public class Letters extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        
+                        Toast.makeText(Letters.this,"Ops, its not ok",Toast.LENGTH_LONG).show();
                     }
                 }
         );
     }
+
+    public void onClickcatBtnListener(){
+        cat_btn = (ImageButton)findViewById(R.id.catBttn);
+        cat_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(Letters.this,"Ops, its not ok",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void onClickelephantBtnListener(){
+        elephant_btn =  (ImageButton)findViewById(R.id.elephantBttn);
+        elephant_btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(Letters.this,"Ops, its not ok",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+
 
 }
