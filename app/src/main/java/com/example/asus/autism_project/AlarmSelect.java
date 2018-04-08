9c5deb9f6c947ccec4d576dace1ee36635f093ab
Yeaseen <1405091.ya@ugrad.cse.buet.ac.bd>
2018-04-08 21:18:59 +0600

    alarm

--- app/src/main/java/com/example/asus/autism_project/AlarmSelect.java
+++ app/src/main/java/com/example/asus/autism_project/AlarmSelect.java
@@ -1,13 +1,36 @@
 package com.example.asus.autism_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmSelect extends AppCompatActivity {
+    public static Button breakfastBttn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_select);
+        setOnClickBreakfastListener();
     }
+
+    public void setOnClickBreakfastListener(){
+        breakfastBttn = (Button)findViewById(R.id.breakfast);
+
+        breakfastBttn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent breakfast = new Intent(AlarmSelect.this,Breakfast.class);
+                        startActivity(breakfast);
+                    }
+                }
+        );
+
+    }
+
+
+
 }
---
+++ app/src/main/java/com/example/asus/autism_project/Breakfast.java
@@ -0,0 +1,13 @@
+package com.example.asus.autism_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Breakfast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breakfast);
+    }
+}
